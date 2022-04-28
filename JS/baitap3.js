b1557c80a2057d8f85420736d09ba428f3a5dab3
doanh11 <trancongdoanh150896@gmail.com>
2022-04-28 16:25:52 +0700

    fix

--- JS/baitap3.js
+++ JS/baitap3.js
@@ -4,39 +4,53 @@ function checkNumber () {
     var numberOne = +document.getElementById ("soThuNhat").value ;
     var numberTow = +document.getElementById ("soThuHai").value ;
     var numberThree = +document.getElementById ("soThuBa").value ;
+    var countSoLe = 0;
+    var countSoChan= 0 ;
+ 
     
     // B2 : kiểm tra số chẵn số lẻ 
-    // if (numberOne % 2 == 0) {
-    //     document.getElementById ("ketQua1").innerHTML = numberOne + " Là số chẵn";
-    // }else {
-    //     document.getElementById ("ketQua1").innerHTML = numberOne + " Là số lẻ";
-    // }
-    // if (numberTow % 2 == 0) {
-    //     document.getElementById ("ketQua2").innerHTML = numberTow + " Là số chẵn";
-    // }else {
-    //     document.getElementById ("ketQua2").innerHTML = numberTow + " Là số lẻ";
-    // }
-    // if (numberThree % 2 == 0 ) {
-    //     document.getElementById ("ketQua3").innerHTML = numberThree + "Là số chẵn";
+  
+    if (numberOne % 2 == 0) {
+        document.getElementById ("ketQua1").innerHTML = numberOne + " Là số chẵn";
+        countSoChan += 1 ;
+    }else {
+        
+        document.getElementById ("ketQua1").innerHTML = numberOne + " Là số lẻ";
+        countSoLe += 1 ;
+    }
+    if (numberTow % 2 == 0) {
+        document.getElementById ("ketQua2").innerHTML = numberTow + " Là số chẵn";
+        countSoChan += 1 ;
+    }else {
+        document.getElementById ("ketQua2").innerHTML = numberTow + " Là số lẻ";
+        countSoLe += 1;
+    }
+    if (numberThree % 2 == 0 ) {
+        document.getElementById ("ketQua3").innerHTML = numberThree + "Là số chẵn";
+        countSoChan += 1 ;
+    }else {
+        document.getElementById ("ketQua3").innerHTML = numberThree + " Là số lẻ";
+        countSoLe += 1 ;
+    }
+   
+    console.log('countSoLe: ', countSoLe)
+    // (numberOne % 2 == 0) ? 
+    //  document.getElementById ("ketQua1").innerHTML = numberOne + " Là số chẵn" :
+    // document.getElementById ("ketQua1").innerHTML = numberOne + " Là số lẻ";
 
-    // }else {
-    //     document.getElementById ("ketQua3").innerHTML = numberThree + " Là số lẻ";
+    // (numberTow % 2 == 0) ? 
+    // document.getElementById ("ketQua2").innerHTML = numberTow + " Là số chẵn" : 
+    // document.getElementById ("ketQua2").innerHTML = numberTow + " Là số lẻ";
 
-    // }
-    (numberOne % 2 == 0) ? 
-     document.getElementById ("ketQua1").innerHTML = numberOne + " Là số chẵn" :
-    document.getElementById ("ketQua1").innerHTML = numberOne + " Là số lẻ";
-
-    (numberTow % 2 == 0) ? 
-    document.getElementById ("ketQua2").innerHTML = numberTow + " Là số chẵn" : 
-    document.getElementById ("ketQua2").innerHTML = numberTow + " Là số lẻ";
-
-    (numberThree % 2 == 0 ) ?
-    document.getElementById ("ketQua3").innerHTML = numberThree + " Là số chẵn" :
-    document.getElementById ("ketQua3").innerHTML = numberThree + " Là số lẻ";
+    // (numberThree % 2 == 0 ) ?
+    // document.getElementById ("ketQua3").innerHTML = numberThree + " Là số chẵn" :
+    // document.getElementById ("ketQua3").innerHTML = numberThree + " Là số lẻ";
 
     // B3: xuất kết quả 
     document.getElementById ("soChanSoLe").style.display = "block" ;
+    document.getElementById ("ketQua4").innerHTML = `<span>Tong chan: ${countSoChan}</span><br/>
+    <span>Tong le: ${countSoLe}</span>
+    `
     
 
     
